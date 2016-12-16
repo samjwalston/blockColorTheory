86e8087759661ccb3623518237387f09531d278f
Sam Walston <samjwalston@gmail.com>
2016-12-16 12:02:30 -0800

    Updated structure of js

--- index.js
+++ index.js
@@ -1,131 +1,208 @@
-;(function() {
-  var input;
-  var button;
-  var inputContainer;
-  var blockContainer;
-  var blocks = [];
-  var count;
-  var factors;
-  var timer;
-
-
-  function updateBlock(element, color) {
-    element.style.backgroundColor = color;
-    element.setAttribute('data-changed', 't');
-
-    return element;
+var Block = (function() {
+  function getBackgroundColor() {
+    return '#' + Math.floor(Math.random() * 0xffffff).toString(16);
   }
 
-  function addBlocksToQueue(index, color) {
-    var top = blocks[index - factors.columns];
-    var right = blocks[index + 1];
-    var bottom = blocks[index + factors.columns];
-    var left = blocks[index - 1];
-    var queue = [];
-
-    if (index >= factors.columns && top.getAttribute('data-changed') !== 't') {
-      queue.push(updateBlock(top, color));
-    }
-
-    if (index % factors.columns !== (factors.columns - 1) && right.getAttribute('data-changed') !== 't') {
-      queue.push(updateBlock(right, color));
-    }
-
-    if (index < (count - factors.columns) && bottom.getAttribute('data-changed') !== 't') {
-      queue.push(updateBlock(bottom, color));
-    }
-
-    if (index % factors.columns !== 0 && left.getAttribute('data-changed') !== 't') {
-      queue.push(updateBlock(left, color));
-    }
-
-    return queue;
-  }
-
-  function updateBlocksInQueue(queue, color) {
-    var upcomingQueue = [];
-
-    for (var i = 0; i < queue.length; ++i) {
-      var index = blocks.indexOf(queue[i]);
-
-      upcomingQueue = upcomingQueue.concat(addBlocksToQueue(index, color));
-    }
-
-    if (upcomingQueue.length) {
-      setTimeout(function() { updateBlocksInQueue(upcomingQueue, color); }, timer);
-    } else if (!document.querySelectorAll('.block[data-changed="f"]').length) {
-      setTimeout(resetBoard, 5000);
-    }
-  }
-
-  function blockClicked(self) {
-    self.setAttribute('data-changed', 't');
-
-    updateBlocksInQueue([self], self.style.backgroundColor);
-  }
-
-  function selectColor() {
-    // Selects a random number from 0 to 1 then times that by 16,777,215 then
-    // rounds down the result to the nearest whole number then converts that
-    // to a stringified hexadecimal number.
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-  }
-
-  function buildBlock(width, height) {
+  function buildSingleBlock(width, height) {
     var element = document.createElement('div');
 
     element.className = 'block';
     element.style.width = width;
     element.style.height = height;
-    element.style.backgroundColor = selectColor();
+    element.style.backgroundColor = getBackgroundColor();
     element.setAttribute('data-changed', 'f');
 
     return element;
   }
 
-  function getFactors() {
-    var factor = Math.floor(Math.sqrt(count));
-    var other = count / factor;
+  function updateSingleBlock(block, color) {
+    block.style.backgroundColor = color;
+    block.setAttribute('data-changed', 't');
+  }
 
-    while (other % 1 !== 0) {
-      factor -= 1;
-      other = count / factor;
+  function getUnchangedAdjacentBlocks(block, color) {
+    var list = Blocks.list();
+    var columns = Blocks.columns();
+    var index = list.indexOf(block);
+    var top = list[index - columns];
+    var right = list[index + 1];
+    var bottom = list[index + columns];
+    var left = list[index - 1];
+    var queue = [];
+
+    if (index >= columns && top && top.getAttribute('data-changed') !== 't') {
+      updateSingleBlock(top, color);
+      queue.push(top);
     }
 
-    return {
-      columns: factor,
-      rows: other
+    if (index % columns !== (columns - 1) && right && right.getAttribute('data-changed') !== 't') {
+      updateSingleBlock(right, color);
+      queue.push(right);
+    }
+
+    if (index < (Blocks.count() - columns) && bottom && bottom.getAttribute('data-changed') !== 't') {
+      updateSingleBlock(bottom, color);
+      queue.push(bottom);
+    }
+
+    if (index % columns !== 0 && left && left.getAttribute('data-changed') !== 't') {
+      updateSingleBlock(left, color);
+      queue.push(left);
+    }
+
+    return queue;
+  }
+
+
+  return {
+    build: buildSingleBlock,
+    update: updateSingleBlock,
+    adjacent: getUnchangedAdjacentBlocks
+  }
+}());
+
+
+var Blocks = (function() {
+  var list = [];
+  var count = 0;
+  var columns = 0;
+  var rows = 0;
+
+
+  function setValues(value) {
+    var num = parseInt(value);
+
+    count = num > 0 ? num : 1;
+    columns = Math.floor(Math.sqrt(count));
+    rows = count / columns;
+
+    while (rows % 1 !== 0) {
+      columns -= 1;
+      rows = count / columns;
     }
   }
 
-  function createBlocks() {
+  function getOrSetCount(value) {
+    if (typeof value === 'undefined') {
+      return count;
+    } else {
+      setValues(value);
+    }
+  }
+
+  function getList() {
+    return list;
+  }
+
+  function getColumns() {
+    return columns;
+  }
+
+  function getRows() {
+    return rows;
+  }
+
+  function buildAllBlocks() {
     var fragment = document.createDocumentFragment();
-    var value = parseInt(input.value);
+    var width = (100 / columns) + '%';
+    var height = (100 / rows) + 'vh';
+    var blocksLeft = count;
 
-    count = value = value > 0 ? value : 1;
-    factors = getFactors();
-    timer = 500 / Math.max(factors.columns, factors.rows);
-
-    var width = (100 / factors.columns) + '%';
-    var height = (100 / factors.rows) + 'vh';
-
-    while (value > 0) {
-      var block = buildBlock(width, height);
+    while (blocksLeft > 0) {
+      var block = Block.build(width, height);
 
       fragment.appendChild(block);
-      blocks.push(block);
+      list.push(block);
 
-      value -= 1;
+      blocksLeft -= 1;
     }
 
-    inputContainer.classList.toggle('hidden');
-    blockContainer.classList.toggle('hidden');
-    blockContainer.appendChild(fragment);
+    return fragment;
   }
 
-  function resetBoard() {
+  function updateAllBlocks(queue, color) {
+    var upcomingQueue = [];
+    var timer = 500 / Math.max(columns, rows);
+
+    console.log(timer);
+
+    for (var i = 0; i < queue.length; ++i) {
+      var adjacentBlocks = Block.adjacent(queue[i], color);
+      upcomingQueue = upcomingQueue.concat(adjacentBlocks);
+    }
+
+    if (upcomingQueue.length) {
+      setTimeout(function() {
+        updateAllBlocks(upcomingQueue, color);
+      }, timer);
+    } else if (!document.querySelectorAll('.block[data-changed="f"]').length) {
+      setTimeout(Page.reset, 5000);
+    }
+  }
+
+  function deleteAllBlocks() {
+    list = [];
+    count = 0;
+    columns = 0;
+    rows = 0;
+  }
+
+
+  return {
+    list: getList,
+    count: getOrSetCount,
+    rows: getRows,
+    columns: getColumns,
+    build: buildAllBlocks,
+    update: updateAllBlocks,
+    delete: deleteAllBlocks
+  }
+}());
+
+
+var Page = (function() {
+  var input;
+  var button;
+  var inputContainer;
+  var blockContainer;
+  var footer;
+
+
+  function clickHandler(event) {
+    var target = event.target;
+
+    if (target === button && !Blocks.list.length) {
+      footer.classList.toggle('hidden');
+      inputContainer.classList.toggle('hidden');
+      blockContainer.classList.toggle('hidden');
+      blockContainer.appendChild(Blocks.build());
+    } else if (target.className === 'block' && target.getAttribute('data-changed') === 'f') {
+      Block.update(target, target.style.backgroundColor);
+      Blocks.update([target], target.style.backgroundColor);
+    }
+  }
+
+  function keyupHandler(event) {
+    var key = event.which || event.keyCode;
+    var active = document.activeElement;
+
+    if (key === 13 && active !== button && input.value) {
+      clickHandler({ target: button });
+    } else if (key === 49 && active !== input) {
+      input.select();
+    } else if (key === 27) {
+      resetPage();
+    } else if (event.target === input) {
+      Blocks.count(input.value);
+    }
+  }
+
+  function resetPage() {
+    Blocks.delete();
+
     input.value = '';
 
+    footer.classList.toggle('hidden');
     inputContainer.classList.toggle('hidden');
     blockContainer.classList.toggle('hidden');
 
@@ -134,43 +211,14 @@
     }
 
     input.select();
-
-    blocks = [];
-    count = undefined;
-    factors = undefined;
-    timer = undefined;
   }
 
-  function clickHandler(event) {
-    var target = event.target;
-
-    if (target === button && !blocks.length) {
-      createBlocks();
-    } else if (target.className === 'block' && target.getAttribute('data-changed') === 'f') {
-      blockClicked(target);
-    }
-  }
-
-  function keyupHandler(event) {
-    var key = event.which || event.keyCode || undefined;
-    var active = document.activeElement;
-
-    if (key === 13 && active !== button) {
-      clickHandler({ target: button });
-    } else if (key === 49 && active !== input) {
-      input.select();
-    } else if (key === 27) {
-      resetBoard();
-    }
-  }
-
-  function initialize() {
-    document.removeEventListener('DOMContentLoaded', initialize);
-
-    input = document.getElementById('count');
-    button = document.getElementById('button');
-    inputContainer = document.getElementById('input');
-    blockContainer = document.getElementById('container');
+  function initializePage() {
+    input = document.getElementById('input');
+    button = document.getElementById('create-blocks');
+    inputContainer = document.getElementById('input-container');
+    blockContainer = document.getElementById('blocks-container');
+    footer = document.getElementById('footer');
 
     document.body.addEventListener('click', clickHandler);
     document.body.addEventListener('keyup', keyupHandler);
@@ -179,5 +227,10 @@
   }
 
 
-  document.addEventListener('DOMContentLoaded', initialize);
+  return {
+    reset: resetPage,
+    initialize: initializePage
+  }
 }());
+
+document.addEventListener('DOMContentLoaded', Page.initialize);
